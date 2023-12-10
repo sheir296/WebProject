b7b87f8b853294b8ac28fb7c906854ce46df28ce
sheir296 <114895375+sheir296@users.noreply.github.com>
2023-12-10 14:58:54 +0500

    props for LOGIN
    created props for login which decides when to show login form and when to hide

--- app-format.js
+++ app-format.js
@@ -1,67 +1,85 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
-import AdbIcon from '@mui/icons-material/Adb';
+import {React,useState,useEffect} from "react"
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
+import { BookList } from "./BookList/BookList"  
+import { LoginDialog } from './Login/login';
 import {
   AppBar,
   Box,
   Toolbar,
-  IconButton,
-  Menu,
-  Container,
   Avatar,
   Button,
-  Tooltip,
-  MenuItem,
+  Container,
   Typography,
 } from '@mui/material';
 
 export const AppFormat = () => {
+  //logic for login dialog box
+const [openLoginDialog, setOpenLoginDialog] = useState(false)
+// function for loginsubmit
+const handleLoginSubmit = (username, password) =>
+{
+  console.log('Login Submitted:',username,password)
+  setOpenLoginDialog(false)
+
+}
+const handleLoginClose=() =>
+{
+  setOpenLoginDialog(false)
+}
   return (
-    <Router>
-      <>
-        <AppBar position="static"   sx={{ backgroundColor:'green'}}>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters >
-              {/* <AdbIcon sx={{ display: 'flex', mr: 1 }} /> */}
-              <Avatar
-                alt="Library Management System"
-                src="client\icons\download.jpeg"
-                sx={{ width: 50, height: 50, marginRight: 1 }}
-              />
-              <Link to="/" style={{ textDecoration: 'none', flexGrow: 5 }}>
-                <Typography
-                  variant="h5"
-                  noWrap
-                  sx={{   //custom css
-                    mr: 3,
-                    display: 'flex',
-                    fontFamily: 'revert-layer',
-                    fontWeight: 900,
-                    letterSpacing: '.2rem',
-                    color: 'black',
-                  }}
-                >
-                  Library Management System
-                </Typography>
-              </Link>
-              <Box
+    <>
+      <AppBar position="static" sx={{ backgroundColor: 'green' }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Avatar
+              alt="Library Management System"
+              src="client\icons\download.jpeg"
+              sx={{ width: 50, height: 50, marginRight: 1 }}
+            />
+            <Link to="/" style={{ textDecoration: 'none', flexGrow: 5 }}>
+              <Typography
+                variant="h5"
+                noWrap
                 sx={{
-                  flexGrow: 0,
+                  mr: 3,
+                  display: 'flex',
+                  fontFamily: 'revert-layer',
+                  fontWeight: 900,
+                  letterSpacing: '.2rem',
+                  color: 'black',
                 }}
               >
-                <Button
-                  onClick={() => {
-                    // setOpenLoginDialog(true)
-                  }}
-                  sx={{ my: 2, color: 'black', display: 'block', fontWeight: 'bold' }}
-                >
-                  Login
-                </Button>
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </>
-    </Router>
+                Library Management System
+              </Typography>
+            </Link>
+            <Box
+              sx={{
+                flexGrow: 0,
+              }}
+            >
+              <Button
+                onClick={() => {
+                   setOpenLoginDialog(true)
+                }}
+                sx={{ my: 2, color: 'black', display: 'block', fontWeight: 'bold' }}
+              >
+                Login
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Routes>
+        <Route path="/books"  element={<BookList />} />
+        {/* fallback route handle 404-like scenarios  */}
+        <Route path="*" element={<Navigate to="/books"  
+         replace />} />
+      </Routes>
+      <LoginDialog
+      open={openLoginDialog}
+      handleSubmit={handleLoginSubmit}
+      handleClose={handleLoginClose} />
+
+    </>
   );
 };
